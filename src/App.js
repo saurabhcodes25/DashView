1fc3fc8560f5c9816016a9f736b1c465d8820d21
kiranpjc7@gmail.com <saurabhjha2001@gmail.com>
2024-07-01 10:32:15 +0530

    grids+flexbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
+
+function Grid() {
+  return (
+    <Container>
+      <Row className='border border-1'>
+        <Col xs={6} md={4} className='border border-1'>
+          Header Logo
+        </Col>
+        <Col xs={12} md={8} className='border border-1'>
+          Header Banner
+        </Col>
+        <Col className='border border-1'>
+          Header Bar
+        </Col>
+
+      </Row>
+
+
+
+      <br></br>
+      <Row className='border border-1'>
+        <Col className='border border-1'>
+          Left Blocks
+        </Col>
+
+
+        <Col className='border border-1'>
+          <Row className='border border-1 '>
+            <div className='text-center'>
+              Center-Center Blocks
+            </div>
+          </Row>
+
+          <Row className='border border-1'>
+            <Col className='border border-1'>Center-Left Blocks</Col>
+            <Col className='border border-1'>Center-Right Blocks</Col>
+          </Row>
+
+          <Row className='border border-1'>
+            <div className='text-center'>
+
+              Module Contents
+            </div>
+          </Row>
+        </Col>
+
+
+        <Col className='border border-1'>
+          Right Blocks
+        </Col>
+      </Row>
+
+
+
+
+      <br></br>
+      <Row className='border border-1'>
+        <Col className='border border-1'>
+          Footer Bar
+        </Col>
+
+      </Row>
+
+
+    </Container>
+
+
+  )
+}
+
+
+function Flex() {
+  return (
+    <>
+
+      {/* upper */}
+
+      <Container fluid className='border border-1'>
+        <Container fluid className='d-flex border border-1'>
+          <div className=' border border-1 w-25' >Header Logo</div>
+          <div className='border border-1 w-75'>Header Banner</div>
+        </Container>
+        <div className=' border border-1'>Header Bar</div>
+      </Container>
+
+
+      <br></br>
+      <br></br>
+
+
+
+      {/*middle*/}
+
+      <Container fluid className='d-flex border border-1 '>
+
+        <Container fluid className='text-center border border-1 '>
+          <div >Left Blocks</div>
+        </Container>
+
+
+        <Container fluid className='border border-1'>
+          <div className='border border-1'>Center-Center Blocks</div>
+
+          <Container fluid className='d-flex border border-1'>
+            <Container fluid className=' border border-1 ' >Center Left Blocks</Container>
+            <Container fluid className='border border-1'>Center Right Blocks</Container>
+          </Container>
+          <div className=' border border-1'>Module Contents</div>
+        </Container>
+
+
+
+        <Container fluid className='text-center border border-1'>
+          <div>Right Blocks</div>
+        </Container>
+
+
+      </Container>
+
+
+      <br></br>
+      <br></br>
+
+
+
+      {/* lower one */}
+
+      <Container fluid className='border border-1'>
+        <Container fluid className='border border-1'>
+          Footer Bar
+        </Container>
+      </Container>
+
+    </>
+
+
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Bootstrap with Grid</h1>
+      <Grid />
+      <h1>React Bootstrap with Flexbox</h1>
+      <Flex />
     </div>
   );
 }
-
 export default App;
